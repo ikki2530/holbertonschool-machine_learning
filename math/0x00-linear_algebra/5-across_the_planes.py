20a1e7e27be991bc62bdc735af9aea6fe37db1df
Diego Gómez <dagomez2530@gmail.com>
2020-12-03 09:35:02 -0500

    Adding copy

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -24,23 +24,34 @@ def add_matrices2D(mat1, mat2):
     mat2: matrix #2 for the add operation
     Returns: A new matrix with the adding element-wise operation of mat1 + mat2
     """
-    shape1 = matrix_shape(mat1)
-    shape2 = matrix_shape(mat2)
-    cols = []
-    mat = []
-    m1 = []
-    m2 = []
-    for row in mat1:
-        m1.append(row.copy())
-    for row in mat2:
-        m2.append(row.copy())
+    # shape1 = matrix_shape(mat1)
+    # shape2 = matrix_shape(mat2)
+    # cols = []
+    # mat = []
+    # m1 = []
+    # m2 = []
+    # for row in mat1:
+    #     m1.append(row.copy())
+    # for row in mat2:
+    #     m2.append(row.copy())
 
-    if shape1 == shape2:
-        for i in range(len(m1)):
-            for j in range(len(m1[0])):
-                cols.append(m1[i][j] + m2[i][j])
-            mat.append(cols)
+    # if shape1 == shape2:
+    #     for i in range(len(m1)):
+    #         for j in range(len(m1[0])):
+    #             cols.append(m1[i][j] + m2[i][j])
+    #         mat.append(cols)
+    #         cols = []
+    #     return mat
+    # else:
+    #     return None
+    if len(mat1) == len(mat2) and len(mat1[0]) == len(mat2[0]):
+        cols = []
+        resultado = []
+        for i in range(len(mat1)):
+            for j in range(len(mat2[0])):
+                cols.append(mat1[i][j] + mat2[i][j])
+            resultado.append(cols)
             cols = []
-        return mat
+        return resultado
     else:
         return None
