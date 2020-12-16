73028ae1d3191bb071bbacac64e965ef64731f50
Diego Gómez <dagomez2530@gmail.com>
2020-12-16 11:06:08 -0500

    Adding new factorial

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -36,9 +36,9 @@ class Poisson():
         if k < 0:
             return 0
         k = int(k)
-        fact = factorial(k)
         e = 2.7182818284590452353602874
-        val_pmf = ((self.lambtha ** k) * (e ** (-1 * self.lambtha)))/factorial(k)
+        val_pmf = ((self.lambtha ** k) * (e ** (
+                  -1 * self.lambtha)))/factorial(k)
         return val_pmf
 
     def cdf(self, k):
@@ -48,9 +48,9 @@ class Poisson():
         Returns: CDF of k
         """
         suma = 0
-        k = int(k)
         if k < 0:
             return 0
+        k = int(k)
         for i in range(k+1):
             suma += self.pmf(i)
         return suma
