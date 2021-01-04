00a004ea3d3ab75c308b7968e0c94a140696e766
Diego Gómez <dagomez2530@gmail.com>
2021-01-03 19:13:53 -0500

    length of layers

--- supervised_learning/0x01-classification/16-deep_neural_network.py
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -18,14 +18,14 @@ class DeepNeuralNetwork():
         if nx < 1:
             raise ValueError("nx must be a positive integer")
 
-        if type(layers) != list:
+        if type(layers) != list or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
 
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
         for i in range(0, self.L):
-            if layers[i] < 1:
+            if layers[i] < 0:
                 raise TypeError("layers must be a list of positive integers")
             if i == 0:
                 self.weights["W" + str(i + 1)] = np.random.randn(
