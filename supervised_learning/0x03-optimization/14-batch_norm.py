128a24bf9ff17cbd9bc145669e98a11bd64c5f48
Diego Gómez <dagomez2530@gmail.com>
2021-01-14 19:32:31 -0500

    Adding batch normalization

--- supervised_learning/0x03-optimization/14-batch_norm.py
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -15,4 +15,15 @@ def create_batch_norm_layer(prev, n, activation):
     - you should use an epsilon of 1e-8.
     Returns: a tensor of the activated output for the layer
     """
+    w = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    model = tf.layers.Dense(units=n, kernel_initializer=w)
+    z = model(prev)
 
+    gamma = tf.Variable(tf.ones((z.get_shape()[-1])))
+    beta = tf.Variable(tf.zeros((z.get_shape()[-1])))
+
+    m, v = tf.nn.moments(z, [0])
+    z_norm = tf.nn.batch_normalization(z, m, v, beta, gamma, 1e-8)
+
+    ypred = activation(z_norm)
+    return ypred
