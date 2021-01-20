2363af84db9411416b86c531e78c0c4c8ba62316
Diego Gómez <dagomez2530@gmail.com>
2021-01-20 18:12:19 -0500

    l2 with tensorflow

--- supervised_learning/0x05-regularization/2-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/2-l2_reg_cost.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 def l2_reg_cost(cost):
     """
     - Calculates the cost of a neural network with L2 regularization
-    Returns: a tensor containing the cost of the network accounting for L2 regularization
+    Returns: a tensor containing the cost of the network
+    accounting for L2 regularization
     """
-    l2_cost = cost + tf.losses.get_regularization_losses()
-    return l2_cost
+    return cost + tf.losses.get_regularization_losses()
