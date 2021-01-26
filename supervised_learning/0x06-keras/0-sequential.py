3ce313d49c3990dcfa02968844a26363c1e11044
Diego Gómez <dagomez2530@gmail.com>
2021-01-26 08:41:38 -0500

    Adding file for task 0

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -20,7 +20,6 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     reg = K.regularizers.l2(lambtha)
     model.add(K.layers.Dense(layers[0], input_shape=(nx,),
               activation=activations[0], kernel_regularizer=reg, name="dense"))
-    model.add(K.layers.Dropout(1 - keep_prob))
     for i in range(1, len(layers)):
         model.add(K.layers.Dropout(1 - keep_prob))
         model.add(K.layers.Dense(layers[i],
