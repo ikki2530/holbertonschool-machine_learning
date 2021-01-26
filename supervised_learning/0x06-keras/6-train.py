a4a754a60359f623fa668b485e402b57b3c7aa67
Diego Gómez <dagomez2530@gmail.com>
2021-01-26 10:20:35 -0500

    Adding file for task 6

---
+++ supervised_learning/0x06-keras/6-train.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Train the model using early stopping
+"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False,
+                patience=0, verbose=True, shuffle=False):
+    """
+    - Trains a model using mini-batch gradient descent.
+    - network is the model to train.
+    - data is a numpy.ndarray of shape (m, nx) containing the input data.
+    - labels is a one-hot numpy.ndarray of shape (m, classes)
+    containing the labels of data.
+    - batch_size is the size of the batch used for mini-batch gradient
+    descent.
+    - epochs is the number of passes through data for mini-batch
+    gradient descent.
+    - validation_data is the data to validate the model with, if not None
+    - early_stopping is a boolean that indicates whether early stopping
+    should be used.
+    - patience is the patience used for early stopping.
+    - verbose is a boolean that determines if output should be printed
+    during training.
+    - shuffle is a boolean that determines whether to shuffle the batches
+    every epoch.
+    """
+    early = None
+    if early_stopping and validation_data is not None:
+        early = K.callbacks.EarlyStopping(monitor="val_loss",
+                                          patience=patience)
+    history = network.fit(x=data, y=labels, epochs=epochs,
+                          batch_size=batch_size,
+                          validation_data=validation_data,
+                          shuffle=shuffle, verbose=verbose, callbacks=[early])
+    return history
