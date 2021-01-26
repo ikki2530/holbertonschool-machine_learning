1236cf64809da1ecc3e6bd171fb9b5e43f86cdca
Diego Gómez <dagomez2530@gmail.com>
2021-01-26 16:08:10 -0500

    Adding file for task 10

---
+++ supervised_learning/0x06-keras/10-weights.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+- saves a model’s weights.
+- loads a model’s weights.
+"""
+import tensorflow.keras as K
+
+
+def save_weights(network, filename, save_format='h5'):
+    """
+    Saves a model’s weights.
+    - network is the model whose weights should be saved.
+    - filename is the path of the file that the weights should be saved to.
+    - save_format is the format in which the weights should be saved.
+    Returns: None.
+    """
+    network.save_weights(filename)
+    return None
+
+
+def load_weights(network, filename):
+    """
+    - Loads a model’s weights.
+    - network is the model to which the weights should be loaded.
+    - filename is the path of the file that the weights
+    should be loaded from.
+    Returns: None
+    """
+    network.load_weights(filename)
+    return None
