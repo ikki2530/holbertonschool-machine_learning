0c480826748d83f4909e6c84f4de3d84920ac53f
Diego Gómez <dagomez2530@gmail.com>
2021-02-04 17:19:10 -0500

    Adding files for task 5

--- supervised_learning/0x07-cnn/5-lenet5.py
+++ supervised_learning/0x07-cnn/5-lenet5.py
@@ -11,18 +11,18 @@ def lenet5(X):
     - X is a K.Input of shape (m, 28, 28, 1) containing the
     input images for the network, m is the number of images.
     """
-    h_ini = K.initializers.he_normal(seed=None)
+    h_ini = K.initializers.he_normal()
     # model = K.Sequential()
     conv1 = K.layers.Conv2D(filters=6, kernel_size=(5, 5), padding="same",
                             activation='relu', kernel_initializer=h_ini)(X)
 
-    pool1 = K.layers.MaxPooling2D(pool_size=(2, 2),
-                                  strides=(2, 2))(conv1)
+    pool1 = K.layers.MaxPool2D(pool_size=(2, 2),
+                               strides=(2, 2))(conv1)
 
     conv2 = K.layers.Conv2D(filters=16, kernel_size=(5, 5), padding="valid",
                             activation='relu', kernel_initializer=h_ini)(pool1)
-    pool2 = K.layers.MaxPooling2D(pool_size=(2, 2),
-                                  strides=(2, 2))(conv2)
+    pool2 = K.layers.MaxPool2D(pool_size=(2, 2),
+                               strides=(2, 2))(conv2)
     flat = K.layers.Flatten()(pool2)
     dns1 = K.layers.Dense(units=120, activation='relu',
                           kernel_initializer=h_ini)(flat)
@@ -33,7 +33,8 @@ def lenet5(X):
 
     model = K.Model(inputs=X, outputs=outp)
 
-    model.compile(optimizer='adam', loss='categorical_crossentropy',
+    model.compile(optimizer=K.optimizers.Adam(),
+                  loss='categorical_crossentropy',
                   metrics=['accuracy'])
 
     return model
