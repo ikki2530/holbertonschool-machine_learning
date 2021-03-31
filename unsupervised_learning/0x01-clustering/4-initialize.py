a15eec4d76e404c609dc473b0290662b5cf2e843
Diego Gómez <dagomez2530@gmail.com>
2021-03-31 11:54:57 -0500

    Comments

--- unsupervised_learning/0x01-clustering/4-initialize.py
+++ unsupervised_learning/0x01-clustering/4-initialize.py
@@ -5,7 +5,17 @@ kmeans = __import__('1-kmeans').kmeans
 
 
 def initialize(X, k):
-    """Initializes variables for a Gaussian Mixture Model"""
+    """Initializes variables for a Gaussian Mixture Model.
+    - X is a numpy.ndarray of shape (n, d) containing the data set
+    - k is a positive integer containing the number of clusters.
+    - Returns: pi, m, S, or None, None, None on failure
+        * pi is a numpy.ndarray of shape (k,) containing the
+        priors for each cluster, initialized evenly.
+        * m is a numpy.ndarray of shape (k, d) containing the
+        centroid means for each cluster, initialized with K-means.
+        * S is a numpy.ndarray of shape (k, d, d) containing the
+        covariance matrices for each cluster, initialized as identity matrices.
+    """
 
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None
