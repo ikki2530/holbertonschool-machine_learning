0b0fd531d8d2d6c8f5d50c9001caf2df5a9cf6f8
Diego Gómez <dagomez2530@gmail.com>
2021-04-15 12:13:35 -0500

    represents a noiseless 1D Gaussian process

---
+++ unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Class GaussianProcess that represents
+a noiseless 1D Gaussian process"""
+
+import numpy as np
+
+
+class GaussianProcess:
+    """represents a noiseless 1D Gaussian process"""
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """Constructor.
+        - X_init is a numpy.ndarray of shape (t, 1) representing
+        the inputs already sampled with the black-box function.
+        - Y_init is a numpy.ndarray of shape (t, 1) representing
+        the outputs of the black-box function for each input in X_init.
+        - t is the number of initial samples.
+        - l is the length parameter for the kernel.
+        - sigma_f is the standard deviation given to the output of
+        the black-box function.
+        """
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        sqdist = np.sum(X_init**2, 1).reshape(-1, 1) +\
+            np.sum(X_init**2, 1) - 2 * np.dot(X_init, X_init.T)
+        self.K = sigma_f**2 * np.exp(-0.5 / l**2 * sqdist)
+
+    def kernel(self, X1, X2):
+        """Method that calculates the covariance
+        kernel matrix between two matrices.
+        - X1 is a numpy.ndarray of shape (m, 1)
+        - X2 is a numpy.ndarray of shape (n, 1)
+        - Returns: the covariance kernel matrix as a numpy.ndarray
+        of shape (m, n).
+        """
+        sqdist = np.sum(X1**2, 1).reshape(-1, 1) +\
+            np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
+        return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
---
+++ unsupervised_learning/0x03-hyperparameter_tuning/README.md
@@ -0,0 +1,44 @@
+# 0x03. Hyperparameter Tuning
+
+## Table of Content
+* [Description](#description)
+* [Learning Objectives](#learning-objectives)
+* [Files Description](#files-description)
+* [Authors](#authors)
+
+## Description
+Concepts of Hyperparameter tuning.
+
+
+## Learning Objectives
+### General
+- What is Hyperparameter Tuning?
+- What is random search? grid search?
+- What is a Gaussian Process?
+- What is a mean function?
+- What is a Kernel function?
+- What is Gaussian Process Regression/Kriging?
+- What is Bayesian Optimization?
+- What is an Acquisition function?
+- What is Expected Improvement?
+- What is Knowledge Gradient?
+- What is Entropy Search/Predictive Entropy Search?
+- What is GPy?
+- What is GPyOpt?
+
+
+
+
+## Files Description
+
+[0-gp.py](0-gp.py) - represents a noiseless 1D Gaussian process.
+[1-gp.py](1-gp.py) - predicts the mean and standard deviation of points in a Gaussian process.
+[2-gp.py](2-gp.py) - updates a Gaussian Process.
+[3-bayes_opt.py](3-bayes_opt.py) - performs Bayesian optimization on a noiseless 1D Gaussian process.
+
+
+
+
+
+## Authors
+* Diego Gomez- [Linkedin](https://www.linkedin.com/in/diego-g%C3%B3mez-8861b61a1/) / [Twitter](https://twitter.com/dagomez2530)
