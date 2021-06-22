3a748c92baf7bed6451382816b871a7c7355735b
Diego Gómez <dagomez2530@gmail.com>
2021-06-22 18:51:03 -0500

    initializes the Q-table

---
+++ reinforcement_learning/0x00-q_learning/1-q_init.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Function that initializes the Q-table"""
+
+import gym
+import numpy as np
+
+
+def q_init(env):
+    """Parameters:
+    env: is the FrozenLakeEnv instance
+    Returns: the Q-table as a numpy.ndarray of zeros"""
+    a_space_size = env.action_space.n
+    s_space_size = env.observation_space.n
+    q_table = np.zeros((s_space_size, a_space_size))
+    return(q_table)
